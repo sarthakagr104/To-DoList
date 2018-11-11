2a9c32b6addd89f7296666b088abde807ab78794
Sarthak <1sarthak.agr@gmail.com>
2018-11-11 17:34:32 +0530

    To-doList

---
+++ js/activity.js
@@ -0,0 +1,70 @@
+define(["sugar-web/activity/activity"], function (activity) {
+
+	// Manipulate the DOM only when it is ready.
+	requirejs(['domReady!'], function (doc) {
+
+		// Initialize the activity.
+		activity.setup();
+		var enterButton = document.getElementById("enter");
+		var input = document.getElementById("userInput");
+		var ul = document.querySelector("ul");
+		var item = document.getElementsByTagName("li");
+
+		function inputLength(){
+			return input.value.length;
+		}
+
+		function listLength(){
+			return item.length;
+		}
+
+		function createListElement() {
+			var li = document.createElement("li");
+			li.appendChild(document.createTextNode(input.value));
+			ul.appendChild(li);
+			input.value = "";
+
+
+
+			function crossOut() {
+				li.classList.toggle("done");
+			}
+
+			li.addEventListener("click",crossOut);
+
+
+
+
+			var dBtn = document.createElement("button");
+			dBtn.appendChild(document.createTextNode("X"));
+			li.appendChild(dBtn);
+			dBtn.addEventListener("click", deleteListItem);
+
+
+
+			function deleteListItem(){
+				li.classList.add("delete")
+			}
+
+		}
+
+
+		function addListAfterClick(){
+			if (inputLength() > 0) {
+				createListElement();
+			}
+		}
+
+		function addListAfterKeypress(event) {
+			if (inputLength() > 0 && event.which ===13) { 
+				createListElement();
+			}
+		}
+
+
+		enterButton.addEventListener("click",addListAfterClick);
+
+		input.addEventListener("keypress", addListAfterKeypress);
+	});
+
+});
